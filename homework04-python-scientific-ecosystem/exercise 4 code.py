21e23f63af372e754eb2cdb955f2d2f71c48a5dd
Peter-zp <62378980+Peter-zp@users.noreply.github.com>
2020-05-06 17:42:00 +0200

    added printed mean values and headlines above values

--- homework04-python-scientific-ecosystem/exercise 4 code.py
+++ homework04-python-scientific-ecosystem/exercise 4 code.py
@@ -1,14 +1,11 @@
 import numpy as np
 
 random1 = np.random.uniform(size=100)
+print("random1 values")
 print(random1)
 random2 = np.random.uniform(size=100)
+print("random2 values")
 print(random2)
-
-random1means = np.mean(random1)
-print(random1means)
-random2means = np.mean(random2)
-print(random2means)
 
 from matplotlib import pyplot as plt
 
@@ -25,6 +22,7 @@ plt.plot(ran_mean2, label = 'mean value')
 plt.show()
 
 random3 = (np.add(random1, random2))/2
+print("random3 values")
 print(random3)
 random3means = np.mean(random3)
 
@@ -33,3 +31,14 @@ plt.title("random number 3")
 ran_mean3 = [np.mean(random3) for i in random3]
 plt.plot(ran_mean3, label = 'mean value')
 plt.show()
+
+random1means = np.mean(random1)
+print("mean of random1")
+print(random1means)
+
+random2means = np.mean(random2)
+print("mean of random2")
+print(random2means)
+random3means = np.mean(random3)
+print("mean of random3")
+print(random3means)
