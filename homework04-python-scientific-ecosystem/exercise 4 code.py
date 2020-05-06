49feeba1fb49502ac399a7ef18b30d9c1457e26f
Peter-zp <62378980+Peter-zp@users.noreply.github.com>
2020-05-06 17:14:36 +0200

    added mean baselines

--- homework04-python-scientific-ecosystem/exercise 4 code.py
+++ homework04-python-scientific-ecosystem/exercise 4 code.py
@@ -14,10 +14,14 @@ from matplotlib import pyplot as plt
 
 plt.plot(random1, label = 'random number 1')
 plt.title('random number 1')
+ran_mean1 = [np.mean(random1) for i in random1]
+plt.plot(ran_mean1, label = 'mean value')
 plt.show()
 
 plt.plot(random2, label = "random number 2")
 plt.title("random number 2")
+ran_mean2 = [np.mean(random1) for i in random2]
+plt.plot(ran_mean2, label = 'mean value')
 plt.show()
 
 random3 = (np.add(random1, random2))/2
@@ -26,4 +30,6 @@ random3means = np.mean(random3)
 
 plt.plot(random3, label = "random number 3")
 plt.title("random number 3")
+ran_mean3 = [np.mean(random3) for i in random3]
+plt.plot(ran_mean3, label = 'mean value')
 plt.show()
